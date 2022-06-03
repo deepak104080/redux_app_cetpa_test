f3e9425aa936c651c30a42cd1e502d769cbbb816
deepak104080 <deepak104080@gmail.com>
2022-06-03 15:32:06 +0530

    3rd June 2022 - Session

--- src/App.js
+++ src/App.js
@@ -3,21 +3,25 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import ProductList from './components/ProductsList';
 import ProductDetail from './components/ProductDetail';
 import Cart from './components/Cart';
+import Orders from './components/Orders';
 
 function App() {
   return (
     <div className="container">
-    <BrowserRouter>
-     <Header/>
-     <Routes>
-       <Route path='/' element={<ProductList/>}/>
-       <Route path='/product/:id' element={<ProductDetail/>}/>
-       <Route path='/cart' element={<Cart/>}/>
-     </Routes>
-     </BrowserRouter>
+      <BrowserRouter>
+        <Header/>
+        <Routes>
+          <Route path='/' element={<ProductList/>}/>
+          <Route path='/product/:id' element={<ProductDetail/>}/>
+          <Route path='/cart' element={<Cart/>}/>
+          <Route path='/orders' element={<Orders/>}/>
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
     </div>
   );
 }
